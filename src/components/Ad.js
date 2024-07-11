1f6bed57282c43ccf54f54c1476c09d2f97ea62b
hoshinodaisuke <78396050+hoshinodaisuke@users.noreply.github.com>
2024-07-11 20:34:37 +0900

    Update Ad.js script

--- src/components/Ad.js
+++ src/components/Ad.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Ad = () => {
     // <script src="//dummy/fr.js?unid=a7799aeb-9d11-4a05-9764-dd6d3efb336b"></script>
-    const iFrame = '<script src="//ad.daoad.com/fr.js?unid=a7799aeb-9d11-4a05-9764-dd6d3efb336b"></script>';
+    const iFrame = '<script src="//ad.daoad.com/fr.js?unid=62c8aca8-1459-4cb8-a564-e969d2ff1c42"></script>';
 
     return (
         <div className="ad-space">
