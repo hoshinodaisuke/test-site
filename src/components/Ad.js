ae27d8e80093568e0386380a63fe00209017ac46
koichirohatayama <koichiro_hatayama@adrea.jp>
2024-10-01 20:01:11 +0900

    Update Ad.js script

--- src/components/Ad.js
+++ src/components/Ad.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Ad = () => {
     // <script src="//dummy/fr.js?unid=a7799aeb-9d11-4a05-9764-dd6d3efb336b"></script>
-    const iFrame = '<script src="//ad.daoad.com/fr.js?unid=62c8aca8-1459-4cb8-a564-e969d2ff1c42"></script>';
+    const iFrame = '<script src="//ad.daoad.com/fr.js?unid=67b394fe-f2ae-4a64-8ba4-7f4b26d4b411"></script>';
 
     return (
         <div className="ad-space">
